e62370bd08a38233130809f7b7f1d0e5010432b9
kogomar <kogomar@gmail.com>
2018-07-26 22:50:06 +0300

    migrate db and make registration

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Маршруты регистрации...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
